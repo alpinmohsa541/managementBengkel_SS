8b4d57e04bbd056a725bdddef4a5ef6df6a0e468
alpinmohsa541 <alpinmohsa@gmail.com>
2024-01-08 13:16:43 +0700

    add modsl Stok,add migration stok and add logo bengkel

---
+++ app/Filament/Resources/StokResource.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\StokResource\Pages;
+use App\Filament\Resources\StokResource\RelationManagers;
+use App\Models\Stok;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class StokResource extends Resource
+{
+    protected static ?string $model = Stok::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                //
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                //
+                Tables\Columns\TextColumn::make('idBarang'),
+                Tables\Columns\TextColumn::make('namaBarang'),
+                Tables\Columns\TextColumn::make('jumlah'),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListStoks::route('/'),
+            'create' => Pages\CreateStok::route('/create'),
+            'edit' => Pages\EditStok::route('/{record}/edit'),
+        ];
+    }
+}
